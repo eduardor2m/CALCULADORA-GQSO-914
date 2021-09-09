9d23e79dc511fc5a15e2382e3ceb81fdcadc858c
Eduardo Melo <deveduardomelo@gmail.com>
2021-09-09 00:44:26 +0000

    feat: sum function created

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const App = express();
+
+const port = 3000;
+
+App.get('/', (req, res) => {
+    res.send('olá mundo')
+})
+
+App.get('/soma/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+
+    if(isNaN(a)){
+        return res.status(400).send('Número inválido')
+    }else if(isNaN(b)){
+        return res.status(400).send('Número inválido')
+    }
+
+    let result = Number(a) + Number(b);
+
+    res.json({
+        result
+    })
+})
+
+App.listen(port, () => {
+    console.log('server running on port', port)
+})
